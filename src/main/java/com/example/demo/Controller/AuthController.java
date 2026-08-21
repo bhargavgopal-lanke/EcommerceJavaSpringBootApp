d0472e3040537be716eb9b6d37386591ffcb4aec
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-08-21 22:20:13 +0530

    basic api structure created

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -1,8 +1,21 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.pojo.SignUpData;
+import com.example.demo.services.AuthServices;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class AuthController {
-
+	
+	@Autowired AuthServices authServices;
+	
+	public String SignupApi(@Valid @RequestBody SignUpData signUpData) {
+		String signUpResponse = authServices.SignupApi(signUpData);
+		return signUpResponse;
+	}
 }
