fcbaebdf68f9f2da434f46336ac9e07edd2c8ecc
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-08-21 22:00:55 +0530

    created new repo for real time apis

---
+++ src/main/java/com/example/demo/EcommercerealTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercerealTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercerealTimeApplication.class, args);
+	}
+
+}
