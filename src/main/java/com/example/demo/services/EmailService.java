710a86b7bb926ccf90fc21b248767cbeb31f4ea1
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2026-08-28 10:27:15 +0530

    created a basic mail service for sending emails

--- src/main/java/com/example/demo/services/EmailService.java
+++ src/main/java/com/example/demo/services/EmailService.java
@@ -1,10 +1,22 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-	 public String sendEmail() {
-		 return "email";
-	 }
+	@Autowired
+	public JavaMailSender javaMailSender;
+
+	public void sendEmail(String fromEmail, String toEmail, String subject, String mailBody) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom(fromEmail);
+		message.setTo(toEmail);
+		message.setSubject(subject);
+		message.setText(mailBody);
+
+		javaMailSender.send(message);
+	}
 }
